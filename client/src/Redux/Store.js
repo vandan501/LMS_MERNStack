d75461b2caa6281c98ffa2d49da74ab3fe43eab1
Raval Vandan <vandanraval2002@gmail.com>
2023-09-07 15:41:18 +0530

    added course slice

--- client/src/Redux/Store.js
+++ client/src/Redux/Store.js
@@ -2,10 +2,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 
 import authSliceReducer from './Slices/AuthSlice';
-
+import courseSliceReducer from "./Slices/CourseSlice";
 const store=configureStore({
     reducer:{
         auth:authSliceReducer,
+        course:courseSliceReducer
     },
     devTools:true
 })
